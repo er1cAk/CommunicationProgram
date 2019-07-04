292dcb03e0c73a5ba6714a0a43fab1bda7890bb6
Erik Dvorčák <erik.dvorcak@student.tuke.sk>
2019-07-04 21:11:47 +0200

    modifying status of inverter

--- modbus_power_plant/Modbus_PowerPlant.cpp
+++ modbus_power_plant/Modbus_PowerPlant.cpp
@@ -5,9 +5,10 @@
 #include "Modbus_PowerPlant.h"
 #include "../constants.h"
 
-ModbusPowerPlant::ModbusPowerPlant(string ip_address, int id): PowerPlant(ip_address, id){
-    modbus1 = modbus(this -> get_ip_address(), MODBUS_DEFAULT_PORT);
+ModbusPowerPlant::ModbusPowerPlant(string ip_address, int id) : PowerPlant(ip_address, id) {
+    modbus1 = modbus(this->get_ip_address(), MODBUS_DEFAULT_PORT);
 }
+
 ModbusPowerPlant::ModbusPowerPlant() = default;
 
 
@@ -18,32 +19,36 @@ bool ModbusPowerPlant::connect() {
 void ModbusPowerPlant::disconnect() {
     try {
         modbus1.modbus_close();
-    } catch (modbus_exception &e){
-        cout<< e.what() <<endl;
+    } catch (modbus_exception &e) {
+        cout << e.what() << endl;
     }
 }
 
 void ModbusPowerPlant::readInvertersData() {
     try {
-        _pstmt = _conn->prepareStatement("SELECT INVERTER_ID, ADDRESS, STATUS_ID, DIVISOR, POWER FROM INVERTERS WHERE PW_ID = ? AND STATUS_ID != 3 AND STATUS_ID != 2 ORDER BY ADDRESS DESC");
-        _pstmt -> setInt(1, this->get_id());
-        _pstmt -> executeUpdate();
-        _res = _pstmt -> executeQuery();
+        _pstmt = _conn->prepareStatement(
+                "SELECT INVERTER_ID, ADDRESS, STATUS_ID, DIVISOR, POWER FROM INVERTERS WHERE PW_ID = ? AND STATUS_ID != 3 AND STATUS_ID != 2 ORDER BY ADDRESS DESC");
+        _pstmt->setInt(1, this->get_id());
+        _pstmt->executeUpdate();
+        _res = _pstmt->executeQuery();
 
         _res->afterLast();
         while (_res->previous()) {
             try {
                 modbus1.modbus_set_slave_id(_res->getInt("ADDRESS"));
                 int id = _res->getInt("INVERTER_ID");
-                int status_id  = _res->getInt("STATUS_ID");
-                if(readInstantPower(id, _res->getInt("DIVISOR"), _res->getInt("POWER") ) > 0){
-                    if(status_id != STATUS_ONLINE){
+                int status_id = _res->getInt("STATUS_ID");
+                int result = readInstantPower(id, _res->getInt("DIVISOR"), _res->getInt("POWER"));
+                if (result > -1) {
+                    if (status_id != STATUS_ONLINE) {
                         this->updateInverterStatus(id, STATUS_ONLINE);
                     }
-                    readDcVoltage(id);
-                    readCurrent(id, _res->getInt("DIVISOR"));
-                }else{
-                    if(status_id != STATUS_COMMUNICATION_ERROR) {
+                    if (result > 0) {
+                        readDcVoltage(id);
+                        readCurrent(id, _res->getInt("DIVISOR"));
+                    }
+                } else {
+                    if (status_id != STATUS_COMMUNICATION_ERROR) {
                         this->updateInverterStatus(id, STATUS_COMMUNICATION_ERROR);
                     }
                 }
@@ -60,16 +65,16 @@ void ModbusPowerPlant::readInvertersData() {
 ssize_t ModbusPowerPlant::readInstantPower(int inverter_id, double divisor, int lastPower) {
     uint16_t buffer[1];
     try {
-        if(modbus1.modbus_read_input_registers(INSTANT_POWER, COUNT_OF_READING_REGISTERS, buffer) == 1){
+        if (modbus1.modbus_read_input_registers(INSTANT_POWER, COUNT_OF_READING_REGISTERS, buffer) == 1) {
             double power = (buffer[0] / divisor) * 1000;
-            if(power == 0 && lastPower == 0){
-                return -1;
+            if (power == 0 && lastPower == 0) {
+                return 0;
             }
             writeDataToDB(inverter_id, power, "INSERT INTO POWER(INVERTER_ID, VALUE ) VALUES(?,?)");
-        }else{
+        } else {
             return -1;
         }
-    } catch (modbus_exception &e){
+    } catch (modbus_exception &e) {
         cout << e.what() << endl;
     }
     return 1;
@@ -80,7 +85,7 @@ int ModbusPowerPlant::readDcVoltage(int inverter_id) {
     try {
         modbus1.modbus_read_input_registers(DC_VOLTAGE, COUNT_OF_READING_REGISTERS, buffer);
         writeDataToDB(inverter_id, buffer[0], "INSERT INTO DC_VOLTAGE(INVERTER_ID, VALUE ) VALUES(?,?)");
-    } catch (modbus_exception &e){
+    } catch (modbus_exception &e) {
         cout << e.what() << endl;
     }
 }
@@ -89,8 +94,9 @@ int ModbusPowerPlant::readCurrent(int inverter_id, double divisor) {
     uint16_t buffer[1];
     try {
         modbus1.modbus_read_input_registers(CURRENT, COUNT_OF_READING_REGISTERS, buffer);
-        writeDataToDB(inverter_id, ((double)(buffer[0]/divisor)), "INSERT INTO CURRENT(INVERTER_ID, VALUE ) VALUES(?,?)");
-    } catch (modbus_exception &e){
+        writeDataToDB(inverter_id, ((double) (buffer[0] / divisor)),
+                      "INSERT INTO CURRENT(INVERTER_ID, VALUE ) VALUES(?,?)");
+    } catch (modbus_exception &e) {
         cout << e.what() << endl;
     }
 }
