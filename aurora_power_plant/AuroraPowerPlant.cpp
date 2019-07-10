3f1732676954151a7ad7d32002ea302221669eb0
Erik Dvorčák <erik.dvorcak@student.tuke.sk>
2019-07-11 01:07:18 +0200

    updating alarms recognition and inverter status

--- aurora_power_plant/AuroraPowerPlant.cpp
+++ aurora_power_plant/AuroraPowerPlant.cpp
@@ -13,10 +13,11 @@ AuroraPowerPlant::AuroraPowerPlant() = default;
 
 void AuroraPowerPlant::readInvertersData() {
     try {
-        _pstmt = _conn->prepareStatement("SELECT INVERTER_ID, ADDRESS, STATUS_ID FROM INVERTERS WHERE PW_ID = ? AND STATUS_ID != 3 ORDER BY ADDRESS DESC");
-        _pstmt -> setInt(1, this->get_id());
-        _pstmt -> executeUpdate();
-        _res = _pstmt -> executeQuery();
+        _pstmt = _conn->prepareStatement(
+                "SELECT INVERTER_ID, ADDRESS, STATUS_ID FROM INVERTERS WHERE PW_ID = ? AND STATUS_ID != 3 ORDER BY ADDRESS DESC");
+        _pstmt->setInt(1, this->get_id());
+        _pstmt->executeUpdate();
+        _res = _pstmt->executeQuery();
 
         _res->afterLast();
         while (_res->previous()) {
@@ -26,22 +27,31 @@ void AuroraPowerPlant::readInvertersData() {
             try {
                 aurora.readState(address);
                 if (aurora.dataState.InverterState == STATE_RUNNING) {
-                    if(status_id != STATUS_ONLINE){
+                    if (status_id != STATUS_ONLINE) {
                         this->updateInverterStatus(id, STATUS_ONLINE);
                     }
                     readGridVoltage(id, address);
                     readGridPower(id, address);
                     readGridCurrent(id, address);
-                }else{
-                    if(aurora.dataState.InverterState == STATE_STANDBY || aurora.dataState.GlobalState == STATE_WAIT_SUN){
+                } else {
+                    if (aurora.dataState.InverterState == STATE_STANDBY ||
+                        aurora.dataState.GlobalState == STATE_WAIT_SUN) {
                         this->updateInverterStatus(id, STATUS_STAND_BY);
                     }
-                    if(aurora.dataState.AlarmState > 0){
-                        this->writeAlarmToDB(id, aurora.dataState.AlarmState, DescriptionAlarmState(aurora.dataState.AlarmState));
+                    if (aurora.dataState.AlarmState > 0) {
+                        this->writeAlarmToDB(id, aurora.dataState.AlarmState,
+                                             DescriptionAlarmState(aurora.dataState.AlarmState));
+                        if (status_id != STATUS_IN_ERROR) {
+                            this->updateInverterStatus(id, STATUS_IN_ERROR);
+                        }
+                    } else {
+                        if (status_id == STATUS_IN_ERROR) {
+                            this->updateInverterStatus(id, STATUS_ONLINE);
+                        }
                     }
                 }
-            } catch (exception &e){
-                if(status_id != 4)
+            } catch (exception &e) {
+                if (status_id != 4)
                     this->updateInverterStatus(id, STATUS_COMMUNICATION_ERROR);
             }
         }
@@ -51,9 +61,9 @@ void AuroraPowerPlant::readInvertersData() {
 }
 
 void AuroraPowerPlant::readGridVoltage(int id, uint8_t address) {
-    if(_connectivity){
+    if (_connectivity) {
         aurora.readDSP(address, GRID_VOLTAGE, GLOBAL_TYPE_GLOBAL);
-        if(aurora.dataDsp.TransmissionState == 0){
+        if (aurora.dataDsp.TransmissionState == 0) {
             writeDataToDB(id, aurora.dataDsp.Value, "INSERT INTO DC_VOLTAGE(INVERTER_ID, VALUE) VALUES(?,?)");
             clearInverterData();
         }
@@ -61,19 +71,19 @@ void AuroraPowerPlant::readGridVoltage(int id, uint8_t address) {
 }
 
 void AuroraPowerPlant::readGridCurrent(int id, uint8_t address) {
-        if(_connectivity){
-            aurora.readDSP(address, GRID_CURRENT, GLOBAL_TYPE_GLOBAL);
-            if(aurora.dataDsp.TransmissionState == 0){
-                writeDataToDB(id, aurora.dataDsp.Value, "INSERT INTO CURRENT(INVERTER_ID, VALUE) VALUES(?,?)");
-                clearInverterData();
-            }
+    if (_connectivity) {
+        aurora.readDSP(address, GRID_CURRENT, GLOBAL_TYPE_GLOBAL);
+        if (aurora.dataDsp.TransmissionState == 0) {
+            writeDataToDB(id, aurora.dataDsp.Value, "INSERT INTO CURRENT(INVERTER_ID, VALUE) VALUES(?,?)");
+            clearInverterData();
         }
+    }
 }
 
 void AuroraPowerPlant::readGridPower(int id, uint8_t address) {
-    if(_connectivity){
-        aurora.readDSP(address, GRID_POWER, GLOBAL_TYPE_GLOBAL );
-        if(aurora.dataDsp.TransmissionState == 0){
+    if (_connectivity) {
+        aurora.readDSP(address, GRID_POWER, GLOBAL_TYPE_GLOBAL);
+        if (aurora.dataDsp.TransmissionState == 0) {
             writeDataToDB(id, aurora.dataDsp.Value, "INSERT INTO POWER(INVERTER_ID, VALUE) VALUES(?,?)");
             clearInverterData();
         }
@@ -94,133 +104,134 @@ void AuroraPowerPlant::disconnect() {
 }
 
 string AuroraPowerPlant::DescriptionAlarmState(uint8_t id) {
-    switch (id){
-        case 2:
+    switch (id) {
+        case (uint8_t) 2:
             return ("Input OC");
-        case 3:
+        case (uint8_t) 3:
             return ("Input UV");
-        case 4:
+        case (uint8_t) 4:
             return ("Input OV");
-        case 5:
+        case (uint8_t) 5:
             return ("Sun Low");
-        case 6:
+        case (uint8_t) 6:
             return ("No Parameters");
-        case 7:
+        case (uint8_t) 7:
             return ("Bulk OV");
-        case 8:
+        case (uint8_t) 8:
             return ("Comm.Error");
-        case 9:
+        case (uint8_t) 9:
             return ("Output OC");
-        case 10:
+        case (uint8_t) 10:
             return ("IGBT Sat");
-        case 11:
+        case (uint8_t) 11:
             return ("Bulk UV");
-        case 12:
+        case (uint8_t) 12:
             return ("Internal error");
-        case 13:
+        case (uint8_t) 13:
             return ("Grid Fail");
-        case 14:
+        case (uint8_t) 14:
             return ("Bulk Low");
-        case 15:
+        case (uint8_t) 15:
             return ("Ramp Fail");
-        case 16:
+        case (uint8_t) 16:
             return ("Dc / Dc Fail");
-        case 17:
+        case (uint8_t) 17:
             return ("Wrong Mode");
-        case 18:
+        case (uint8_t) 18:
             return ("Ground Fault");
-        case 19:
+        case (uint8_t) 19:
             return ("Over Temp.");
-        case 20:
+        case (uint8_t) 20:
             return ("Bulk Cap Fail");
-        case 21:
+        case (uint8_t) 21:
             return ("Inverter Fail");
-        case 22:
+        case (uint8_t) 22:
             return ("Start Timeout");
-        case 23:
+        case (uint8_t) 23:
             return ("Ground Fault");
-        case 24:
+        case (uint8_t) 24:
             return ("Degauss error");
-        case 25:
+        case (uint8_t) 25:
             return ("Ileak sens.fail");
-        case 26:
+        case (uint8_t) 26:
             return ("DcDc Fail");
-        case 27:
+        case (uint8_t) 27:
             return ("Self Test Error 1");
-        case 28:
+        case (uint8_t) 28:
             return ("Self Test Error 2");
-        case 29:
+        case (uint8_t) 29:
             return ("Self Test Error 3");
-        case 30:
+        case (uint8_t) 30:
             return ("Self Test Error 4");
-        case 31:
+        case (uint8_t) 31:
             return ("DC inj error");
-        case 32:
+        case (uint8_t) 32:
             return ("Grid OV");
-        case 33:
+        case (uint8_t) 33:
             return ("Grid UV");
-        case 34:
+        case (uint8_t) 34:
             return ("Grid OF");
-        case 35:
+        case (uint8_t) 35:
             return ("Grid UF");
-        case 36:
+        case (uint8_t) 36:
             return ("Z grid Hi");
-        case 37:
+        case (uint8_t) 37:
             return ("Internal error");
-        case 38:
+        case (uint8_t) 38:
             return ("Riso Low");
-        case 39:
+        case (uint8_t) 39:
             return ("Vref Error");
-        case 40:
+        case (uint8_t) 40:
             return ("Error Meas V");
-        case 41:
+        case (uint8_t) 41:
             return ("Error Meas ");
-        case 42:
+        case (uint8_t) 42:
             return ("Error Meas Z");
-        case 43:
+        case (uint8_t) 43:
             return ("Error Meas Ileak");
-        case 44:
+        case (uint8_t) 44:
             return ("Error Read V");
-        case 45:
+        case (uint8_t) 45:
             return ("Error Read I");
-        case 46:
+        case (uint8_t) 46:
             return ("Table fail");
-        case 47:
+        case (uint8_t) 47:
             return ("Fan Fail");
-        case 48:
+        case (uint8_t) 48:
             return ("UTH");
-        case 49:
+        case (uint8_t) 49:
             return ("Interlock fail");
-        case 50:
+        case (uint8_t) 50:
             return ("Remote Off");
-        case 51:
+        case (uint8_t) 51:
             return ("Vout Avg errror");
-        case 52:
+        case (uint8_t) 52:
             return ("Battery low");
-        case 53:
+        case (uint8_t) 53:
             return ("Clk fail");
-        case 54:
+        case (uint8_t) 54:
             return ("Input UC");
-        case 55:
+        case (uint8_t) 55:
             return ("Zero Power");
-        case 56:
+        case (uint8_t) 56:
             return ("Fan Stucked");
-        case 57:
+        case (uint8_t) 57:
             return ("DC Switch Open");
-        case 58:
+        case (uint8_t) 58:
             return ("Tras Switch Open");
-        case 59:
+        case (uint8_t) 59:
             return ("AC Switch Open");
-        case 60:
+        case (uint8_t) 60:
             return ("Bulk UV");
-        case 61:
+        case (uint8_t) 61:
             return ("Autoexclusion");
-        case 62:
+        case (uint8_t) 62:
             return ("Grid df / dt");
-        case 63:
+        case (uint8_t) 63:
             return ("Den switch Open");
-        case 64:
+        case (uint8_t) 64:
             return ("Jbox fail");
-        default: break;
+        default:
+            break;
     }
 }
